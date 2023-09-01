738b25adc42204dfb8a90794651f84c2423544b7
john <johnmaluki58@gmail.com>
2023-09-01 21:58:51 +0300

    Add Aggregate Methods

--- lib/models.py
+++ lib/models.py
@@ -2,6 +2,8 @@ from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
+from connect import session
+
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }
@@ -17,6 +19,22 @@ class Company(Base):
     founding_year = Column(Integer())
 
     freebies = relationship('Freebie', backref=backref('company'))
+    devs = relationship("Dev", secondary="freebies", back_populates="companies")
+
+    def give_freebie(self, dev, item_name, value):
+        freebie = Freebie(
+            item_name=item_name, 
+            value=value,
+            company_id = self.id,
+            dev_id = dev.id
+        )
+        session.add(freebie)
+        session.commit()
+
+    @classmethod
+    def oldest_company(cls):
+        company = session.query(cls).order_by(cls.founding_year).first()
+        return company
 
     def __repr__(self):
         return f'<Company {self.name}>'
@@ -28,6 +46,21 @@ class Dev(Base):
     name= Column(String())
 
     freebies = relationship('Freebie', backref=backref('dev'))
+    companies = relationship("Company", secondary="freebies", back_populates="devs")
+
+    def give_away(self, dev, freebie):
+        if freebie.dev == self:
+            freebie.dev == dev
+            session.commit()
+        
+
+    @classmethod
+    def received_one(cls, item_name):
+        dev = session.query(cls).filter_by(name=item_name).first()
+        if dev:
+            return True
+        else:
+            return False
 
     def __repr__(self):
         return f'<Dev {self.name}>'
@@ -44,10 +77,9 @@ class Freebie(Base):
     # company = relationship('Company', back_populates='freebies')
     # dev = relationship('Dev', back_populates='freebies')
 
-    @classmethod
-    def print_details():
-        pass
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
 
     def __repr__(self):
 
-        return f'Freebie(id={self.id}'
+        return f'Freebie(id={self.id})'
